d2c705a8c326c22e618e0a83a3435c70de6eedcd
Artur Marbach <Marbach.a@gmx.net>
2022-10-31 15:46:13 +0100

    add task

--- js/add_task.js
+++ js/add_task.js
@@ -1,3 +1,24 @@
-function changeColor(x){
-    document.getElementById(`${x}`).style.backgroundColor = `${x}`;
+let allTasks = [];
+
+/**
+ * Create Task and put it to the JSON allTasks
+ * 
+ */
+function addTask(){
+    let title = document.getElementById('').value;
+    let description = document.getElementById('').value;
+    let category = document.getElementById('').value;
+
+    let task = {
+        'title': title,
+        'description': description,
+        'category' : category,
+        'createAt': new Date().getTime()
+    };
+
+
+    allTasks.push(task);
+
+    let allTasksAsString = JSON.stringify(allTasks);
+    localStorage.setItem('allTasks', allTasksAsString);
 }
