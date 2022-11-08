d5ceaffe07d7c52a66188b9a34f9063f5e659527
A-Marbach <113348931+A-Marbach@users.noreply.github.com>
2022-11-08 10:28:41 +0100

    abc

--- js/add_task.js
+++ js/add_task.js
@@ -5,15 +5,16 @@ let allTasks = [];
  * 
  */
 function addTask(){
-    let title = document.getElementById('').value;
-    let description = document.getElementById('').value;
-    let category = document.getElementById('').value;
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let category = document.getElementById('category').value;
+    let dueDate = document.getElementById('dueDate').value;
 
     let task = {
         'title': title,
         'description': description,
         'category' : category,
-        'createAt': new Date().getTime()
+        'DueDate': dueDate
     };
 
 
@@ -21,4 +22,40 @@ function addTask(){
 
     let allTasksAsString = JSON.stringify(allTasks);
     localStorage.setItem('allTasks', allTasksAsString);
+}
+
+
+function changeColorRed(){
+    document.getElementById('red').classList.add('red');
+    document.getElementById('redImg').src = 'asseds/img/pfeil-oben-weiss.png';
+
+    document.getElementById('yellow').classList.remove('yellow');
+    document.getElementById('yellowImg').src = 'asseds/img/medium-gelb.png';
+
+    document.getElementById('green').classList.remove('green');
+    document.getElementById('greenImg').src = 'asseds/img/pfeil-unten-grün.png';
+}
+
+function changeColorYellow(){
+    document.getElementById('yellow').classList.add('yellow');
+    document.getElementById('yellowImg').src = 'asseds/img/medium-weiss.png';
+
+    document.getElementById('red').classList.remove('red');
+    document.getElementById('redImg').src = 'asseds/img/pfeil-oben-rot.png';
+
+    document.getElementById('green').classList.remove('green');
+    document.getElementById('greenImg').src = 'asseds/img/pfeil-unten-grün.png';
+}
+
+function changeColorGreen(){
+    document.getElementById('green').classList.add('green');
+    document.getElementById('greenImg').src = 'asseds/img/pfeil-unten-weiss.png';
+
+    document.getElementById('red').classList.remove('red');
+    document.getElementById('redImg').src = 'asseds/img/pfeil-oben-rot.png';
+
+    document.getElementById('yellow').classList.remove('yellow');
+    document.getElementById('yellowImg').src = 'asseds/img/medium-gelb.png';
+
+    
 }
