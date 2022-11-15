baa8e445d2d2a24abd84f0486f74ea4d6dbf1e40
A-Marbach <113348931+A-Marbach@users.noreply.github.com>
2022-11-15 08:00:56 +0100

    new

--- js/add_task.js
+++ js/add_task.js
@@ -1,31 +1,45 @@
 let allTasks = [];
+let prio;
+let subtasks = [];
+i = 0;
+setURL('http://developerakademie.com/smallest_backend_ever');
 
-/**
- * Create Task and put it to the JSON allTasks
- * 
- */
-function addTask(){
-    let title = document.getElementById('title').value;
-    let description = document.getElementById('description').value;
-    let category = document.getElementById('category').value;
-    let dueDate = document.getElementById('dueDate').value;
+async function init() {
+    await downloadFromServer();
+    users = JSON.parse(backend.getItem('users')) || [];
+
+}
+
+
+function addTask() {
+    let title = document.getElementById('title');
+    let description = document.getElementById('description');
+    let category = document.getElementById('category');
+    let dueDate = document.getElementById('dueDate');
 
     let task = {
-        'title': title,
-        'description': description,
-        'category' : category,
-        'DueDate': dueDate
+        'title': title.value,
+        'description': description.value,
+        'category': category.value,
+        'dueDate': dueDate.value,
+        'priority': prio,
+        'subtask': subtask,
+        'assigned': assigned
     };
-
-
     allTasks.push(task);
 
     let allTasksAsString = JSON.stringify(allTasks);
     localStorage.setItem('allTasks', allTasksAsString);
 }
 
+function loadAllTasks() {
+    let allTasksAsString = localStorage.getItem('allTasks');
+    allTasks = JSON.parse(allTasksAsString);
+}
 
-function changeColorRed(){
+
+function changeColorRed() {
+    changePrioUrgent();
     document.getElementById('red').classList.add('red');
     document.getElementById('redImg').src = 'asseds/img/pfeil-oben-weiss.png';
 
@@ -36,7 +50,13 @@ function changeColorRed(){
     document.getElementById('greenImg').src = 'asseds/img/pfeil-unten-grün.png';
 }
 
-function changeColorYellow(){
+function changePrioUrgent() {
+    prio;
+    prio = 'urgent';
+}
+
+function changeColorYellow() {
+    changePrioMedium();
     document.getElementById('yellow').classList.add('yellow');
     document.getElementById('yellowImg').src = 'asseds/img/medium-weiss.png';
 
@@ -47,7 +67,13 @@ function changeColorYellow(){
     document.getElementById('greenImg').src = 'asseds/img/pfeil-unten-grün.png';
 }
 
-function changeColorGreen(){
+function changePrioMedium() {
+    prio;
+    prio = 'medium';
+}
+
+function changeColorGreen() {
+    changePrioLow();
     document.getElementById('green').classList.add('green');
     document.getElementById('greenImg').src = 'asseds/img/pfeil-unten-weiss.png';
 
@@ -56,6 +82,74 @@ function changeColorGreen(){
 
     document.getElementById('yellow').classList.remove('yellow');
     document.getElementById('yellowImg').src = 'asseds/img/medium-gelb.png';
-
-    
 }
+
+function changePrioLow() {
+    prio;
+    prio = 'low';
+}
+
+
+function renderSubtask() {
+    let subtask = document.getElementById('subtask').value;
+    document.getElementById('subtask').value = '';
+
+    generateSubtask(subtask, i);
+    i++;
+}
+
+function generateSubtask(subtask, i) {
+
+    return document.getElementById('renderSubtask').innerHTML += `
+    <div class="subtaskContainer">
+    <input onclick="checkSubtask('${subtask}')" class="checkbox" type="checkbox">
+    <p id="getSubtask">${subtask}</p>
+    </div>
+    `;
+}
+
+function checkSubtask(subtask) {
+    subtasks.push(subtask);
+    console.log(subtask);
+}
+
+
+function dropDownCategory() {
+    document.getElementById("category").classList.toggle("show");
+
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function (event) {
+        if (!event.target.matches('.drop')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+}
+
+function dropDownAssign() {
+    document.getElementById("assign").classList.toggle("show");
+
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function (event) {
+        if (!event.target.matches('.drop')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+}
+
+
+
+
