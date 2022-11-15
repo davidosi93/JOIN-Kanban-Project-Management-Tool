0c26929b3db6af19bb0517e083cd836621044c99
A-Marbach <113348931+A-Marbach@users.noreply.github.com>
2022-11-15 15:06:46 +0100

    change add task site

--- js/add_task.js
+++ js/add_task.js
@@ -2,13 +2,20 @@ let allTasks = [];
 let prio;
 let subtasks = [];
 i = 0;
-setURL('http://developerakademie.com/smallest_backend_ever');
 
-async function init() {
-    await downloadFromServer();
-    users = JSON.parse(backend.getItem('users')) || [];
+let categoriesName =['Sales', 'Backoffice'];
+let categoriesImg = ['asseds/img/violetter-kreis.png', 'asseds/img/hellblauer-kreis.png'];
 
-}
+let newCategoryImg =['asseds/img/violetter-kreis.png', 'asseds/img/hellblauer-kreis.png','asseds/img/roter-kreis.png','asseds/img/oranger-kreis.png','asseds/img/blauer-kreis.png'];
+
+// setURL('http://developerakademie.com/smallest_backend_ever');
+
+// async function init() {
+//     await downloadFromServer();
+//     users = JSON.parse(backend.getItem('users')) || [];
+//     renderCategory();
+
+// }
 
 
 function addTask() {
@@ -81,32 +88,33 @@ function changeColorYellow() {
     changePrioMedium();
     document.getElementById('yellow').classList.add('yellow');
     document.getElementById('yellowImg').src = 'asseds/img/medium-weiss.png';
-    function changeColorYellow() {
-        console.log('yellow', yellow)
-
-        if (yellow) {
-            document.getElementById('yellow').classList.remove('yellow');
-            document.getElementById('yellowImg').src = '/asseds/img/medium-gelb.png';
-            red = false;
-            yellow = false;
-            green = false;
-        } else {
-            document.getElementById('yellow').classList.add('yellow');
-            document.getElementById('yellowImg').src = '/asseds/img/medium-weiss.png';
-            yellow = true;
-            red = false;
-            green = false;
-        }
-
-
-
-        document.getElementById('red').classList.remove('red');
-        document.getElementById('redImg').src = '/asseds/img/pfeil-oben-rot.png';
-
-        document.getElementById('green').classList.remove('green');
-        document.getElementById('greenImg').src = '/asseds/img/pfeil-unten-grün.png';
-    }
 }
+function changeColorYellow() {
+    console.log('yellow', yellow)
+
+    if (yellow) {
+        document.getElementById('yellow').classList.remove('yellow');
+        document.getElementById('yellowImg').src = '/asseds/img/medium-gelb.png';
+        red = false;
+        yellow = false;
+        green = false;
+    } else {
+        document.getElementById('yellow').classList.add('yellow');
+        document.getElementById('yellowImg').src = '/asseds/img/medium-weiss.png';
+        yellow = true;
+        red = false;
+        green = false;
+    }
+
+
+
+    document.getElementById('red').classList.remove('red');
+    document.getElementById('redImg').src = '/asseds/img/pfeil-oben-rot.png';
+
+    document.getElementById('green').classList.remove('green');
+    document.getElementById('greenImg').src = '/asseds/img/pfeil-unten-grün.png';
+}
+
 
 function changePrioMedium() {
     prio;
@@ -117,22 +125,23 @@ function changeColorGreen() {
     changePrioLow();
     document.getElementById('green').classList.add('green');
     document.getElementById('greenImg').src = 'asseds/img/pfeil-unten-weiss.png';
-    function changeColorGreen() {
-        console.log('green', green)
-        if (green) {
-            document.getElementById('green').classList.remove('green');
-            document.getElementById('greenImg').src = '/asseds/img/pfeil-unten-grün.png';
-            red = false;
-            yellow = false;
-            green = false;
-        } else {
-            document.getElementById('green').classList.add('green');
-            document.getElementById('greenImg').src = '/asseds/img/pfeil-unten-weiss.png';
-            green = true;
-            red = false;
-            yellow = false;
-        }
+}
+function changeColorGreen() {
+    console.log('green', green)
+    if (green) {
+        document.getElementById('green').classList.remove('green');
+        document.getElementById('greenImg').src = '/asseds/img/pfeil-unten-grün.png';
+        red = false;
+        yellow = false;
+        green = false;
+    } else {
+        document.getElementById('green').classList.add('green');
+        document.getElementById('greenImg').src = '/asseds/img/pfeil-unten-weiss.png';
+        green = true;
+        red = false;
+        yellow = false;
     }
+
 
     document.getElementById('red').classList.remove('red');
     document.getElementById('redImg').src = '/asseds/img/pfeil-oben-rot.png';
@@ -207,3 +216,78 @@ function dropDownAssign() {
     }
 }
 
+
+
+function renderCategory(){
+    let content = document.getElementById('category');
+    
+    for (let i = 0; i < categoriesName.length; i++) {
+        const category = categoriesName[i];
+        const img = categoriesImg[i];
+        content.innerHTML += `
+        
+        <a onclick="myCategory(${i})">${category} <img src="${img}"</a>
+        `;
+        
+    }
+
+}
+
+function myCategory(i){
+    let content = document.getElementById('selectCategory');
+    content.innerHTML = '';
+    content.innerHTML = `
+    <div class="category">
+    ${categoriesName[i]}<img src="${categoriesImg[i]}">
+    </div>
+    <img src="/asseds/img/pfeil unten.png">
+    `;
+}
+
+
+function newCategory(){
+    let content = document.getElementById('selectCategory');
+    
+    content.innerHTML = '';
+    content.innerHTML +=`
+    
+    <input class="newCategoryText" placeholder="New category Name...">
+    <img onclick="goToSelectCategory()" class="xBtn" src="/asseds/img/x-schwarz.png">
+    <img class="hackenBtn" src="/asseds/img/hacken-schwarz.png">
+    `;
+
+    let contentImg = document.getElementById('category');
+    contentImg.innerHTML = '';
+    contentImg.style.display="block";
+    for (let i = 0; i < newCategoryImg.length; i++) {
+        const img = newCategoryImg[i];
+        contentImg.innerHTML +=`
+        <img onclick="newCategoryImages(${i})" class="newCategoryImg" src="${img}">
+        `;
+    }
+}
+
+
+function newCategoryImages(i){
+    let content = document.getElementById('selectCategory');
+    
+    content.innerHTML = '';
+    content.innerHTML +=`
+    <img src="${newCategoryImg[i]}">
+    <input "required"  class="newCategoryText"  placeholder="New category Name...">
+    
+    <img onclick="goToSelectCategory()" class="xBtn" src="/asseds/img/x-schwarz.png">
+    <img class="hackenBtn" src="/asseds/img/hacken-schwarz.png">
+    `;
+}
+
+
+function goToSelectCategory(){
+    let content = document.getElementById('selectCategory');
+    let input = document.getElementById('category').innerHTML = '';
+    content.innerHTML=`
+    Select task category<img src="/asseds/img/pfeil unten.png">
+    `;
+    renderCategory();
+   
+}
