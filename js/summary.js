5a51818c2a1b3720651a4f669e6e7b9fba794b42
A-Marbach <113348931+A-Marbach@users.noreply.github.com>
2023-02-11 16:22:07 +0100

    abc

--- js/summary.js
+++ js/summary.js
@@ -1,36 +1,37 @@
-function help(){
+function help() {
     document.getElementById('mainContainer').classList.add('d-none');
     document.getElementById('help').classList.remove('d-none');
 }
 
-function goBacktoMainContainer(){
+function goBacktoMainContainer() {
     document.getElementById('mainContainer').classList.remove('d-none');
     document.getElementById('help').classList.add('d-none');
 }
 
-function onHoverLeft(){
+function onHoverLeft() {
     document.getElementById('left-img').src = 'asseds/img/stift-weiss.png';
 }
 
-function offHoverLeft(){
+function offHoverLeft() {
     document.getElementById('left-img').src = 'asseds/img/Frame 59.png';
 }
 
-function onHoverRight(){
+function onHoverRight() {
     document.getElementById('right-img').src = 'asseds/img/hacken-weiss.png';
-    
+
 }
 
-function offHoverRight(){
+function offHoverRight() {
     document.getElementById('right-img').src = 'asseds/img/Group 7 (1).png';
 }
 
-async function initSummary(){
+async function initSummary() {
     includeHTML();
     await getAllTasks();
     filterAllTasks();
     watchTask();
     loadActiveUsers();
+    
 }
 
 function loadActiveUsers() {
@@ -51,31 +52,47 @@ function loadActiveUsers() {
             </div>
         `;
 
-
+greet(name);
 }
-function watchTask(){
+function watchTask() {
     let watchBoard = document.getElementById('task-board');
     let watchTodo = document.getElementById('task-todo');
     let watchDone = document.getElementById('task-done');
     let watchProgress = document.getElementById('task-progress');
     let watchFeedback = document.getElementById('task-feedback');
-    clearDivs(watchBoard,watchTodo,watchDone,watchProgress,watchFeedback);
-    watchtasksLength(watchBoard,watchTodo,watchDone,watchProgress,watchFeedback);
-    
+    clearDivs(watchBoard, watchTodo, watchDone, watchProgress, watchFeedback);
+    watchtasksLength(watchBoard, watchTodo, watchDone, watchProgress, watchFeedback);
+
 }
 
-function  clearDivs(watchBoard,watchTodo,watchDone,watchProgress,watchFeedback){
+function clearDivs(watchBoard, watchTodo, watchDone, watchProgress, watchFeedback) {
     watchTodo.innerHTML = '';
     watchDone.innerHTML = '';
-    watchBoard.innerHTML ='';
-    watchProgress.innerHTML ='';
-    watchFeedback.innerHTML ='';
+    watchBoard.innerHTML = '';
+    watchProgress.innerHTML = '';
+    watchFeedback.innerHTML = '';
 }
 
-function watchtasksLength(watchBoard,watchTodo,watchDone,watchProgress,watchFeedback){
+function watchtasksLength(watchBoard, watchTodo, watchDone, watchProgress, watchFeedback) {
     watchBoard.innerHTML += `<b>${allTasks.length}</b>`;
     watchProgress.innerHTML += `<b>${progress.length}</b>`;
     watchFeedback.innerHTML += `<b>${feedback.length}</b>`;
     watchTodo.innerHTML += `<b>${todo.length}</b>`;
     watchDone.innerHTML += `<b>${done.length}</b>`;
+}
+
+
+function greet(name) {
+    let greet = document.getElementById('greetTime');
+    greet.innerHTML = '';
+    let today = new Date()
+    let curHr = today.getHours()
+
+    if (curHr < 12) {
+        greet.innerHTML =`good morning<br><p>${name}</p>`;
+    } else if (curHr < 18) {
+                greet.innerHTML =`good afternoon<br><p>${name}</p>`;
+    } else {
+                greet.innerHTML =`good evening<br><p>${name}</p>`;
+    }
 }
